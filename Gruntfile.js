e394f7eed3fb6b35b6a110a8d3e5114480bcae19
Jose Ricardo Xavier <jrxavier@gmail.com>
2017-04-05 21:50:06 -0300

    Termino cap10

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,42 @@ module.exports = function(grunt) {
                 dest: 'dist'
 
             }
-        }
+        },
+        clean: {
+            dist: {
+                src: 'dist'
+            }
+        },
 
+        usemin: {
+            html: 'dist/app/views/**/*.ejs'
+        },
+
+        useminPrepare: {
+            options: {
+                root: 'dist/public',
+                dest: 'dist/public'
+            },
+            html: 'dist/app/views/**/*.ejs'
+        },
+        ngAnnotate: {
+            scripts: {
+                expand: true,
+                src: ['dist/public/js/**/*.js']
+            }
+        }
     });
 
+    grunt.registerTask('default', ['dist', 'mimifica']);
+    grunt.registerTask('dist', ['clean', 'copy']);
+    grunt.registerTask('mimifica', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
+
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-ng-annotate');
 
 };
